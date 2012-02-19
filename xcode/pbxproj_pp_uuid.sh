13985b35980964493671b73a8c403a3fc43cc5c9
Jerome Lacoste <jerome.lacoste@gmail.com>
2012-02-19 14:47:59 +0100

    pbxproj_pp_uuid.sh cleanup and add error handling

--- xcode/pbxproj_pp_uuid.sh
+++ xcode/pbxproj_pp_uuid.sh
@@ -3,6 +3,12 @@
 # A script to extract the provisioning profile UUID from a XCode .pbxproj files given its configuration name
 #
 debug=0
+plistBuddy=/usr/libexec/PlistBuddy
+
+if [ ! -x $plistBuddy ]; then
+	echo "ERROR PlistBuddy not found in expected location"
+	exit 1
+fi
 
 function usage() {
 	echo "USAGE: $0 file configuration value"
@@ -23,40 +29,57 @@ if [ $# -lt 2 ]; then
 fi
 file=$1
 configuration=$2
-value=$3
 
-plistBuddy=/usr/libexec/PlistBuddy
+if [ ! -f $file ]; then
+	echo "ERROR: $file file not found"
+	usage
+	exit 1
+fi
 
 # 1- find the matching build configuration
-
-rootObj=`$plistBuddy $file -c "Print rootObject"`
+rootObj=`$plistBuddy $file -c "Print rootObject" 2>/dev/null`
+if [ $? -ne 0 ]; then
+	echo "ERROR: couldn't find rootObject in file '${file}'. Is the file an XCode project file ?"
+	exit 1
+fi
 debug "rootObj $rootObj"
-buildConfigurationList=`$plistBuddy $file -c "Print objects:$rootObj:buildConfigurationList"`
+buildConfigurationList=`$plistBuddy $file -c "Print objects:$rootObj:buildConfigurationList" 2>/dev/null`
+if [ $? -ne 0 ]; then
+	echo "ERROR: couldn't find buildConfigurationList in file '${file}'"
+	exit 1
+fi
 debug "buildConfigurationList $buildConfigurationList"
-x1=`$plistBuddy $file -c "Print objects:$buildConfigurationList:buildConfigurations:size"`
 
 buildConfigurationObjId=
 idx=0
-
 while [ 1 ]; do
 	x=`$plistBuddy $file -c "Print objects:$buildConfigurationList:buildConfigurations:$idx" 2>/dev/null`
 	if [ $? -ne 0 ]; then
 		break
 	fi
-	namex=`$plistBuddy $file -c "Print objects:${x}:name"`
+	namex=`$plistBuddy $file -c "Print objects:${x}:name" 2>/dev/null`
+	if [ $? -ne 0 ]; then
+		echo "ERROR: couldn't find buildConfiguration name for object '${x}'"
+		exit 1
+	fi
 	debug "Configuration: $namex"
 	if [ "$namex" == "$configuration" ]; then
 		buildConfigurationObjId=$x
+		break
 	fi
 	let "idx++"
 done
 
 debug "buildConfigurationObjId for $configuration: ${buildConfigurationObjId}"
 
+uuid=`$plistBuddy $file -c "Print objects:${buildConfigurationObjId}:buildSettings:PROVISIONING_PROFILE[sdk=iphoneos*]" 2>/dev/null`
+if [ $? -ne 0 ]; then
+	echo "ERROR: couldn't find uuid name for object '${buildConfigurationObjId}'"
+	exit 1
+fi
+debug "uuid $uuid"
 #codeSign=`$plistBuddy $file -c "Print objects:${buildConfigurationObjId}:buildSettings:CODE_SIGN_IDENTITY[sdk=iphoneos*]"`
-provProfile=`$plistBuddy $file -c "Print objects:${buildConfigurationObjId}:buildSettings:PROVISIONING_PROFILE[sdk=iphoneos*]" 2>/dev/null`
 #debug "codeSign $codeSign"
-debug "provProfile $provProfile"
 
 # 2- display the UUID
-echo "$provProfile"
+echo "$uuid"
