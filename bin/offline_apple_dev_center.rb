2f6611f15b1259e58c6c18cc8d2ed127eff00426
Jerome Lacoste <jerome.lacoste@gmail.com>
2013-02-05 12:07:04 +0100

    Some troubleshooting code for profile finder

--- bin/offline_apple_dev_center.rb
+++ bin/offline_apple_dev_center.rb
@@ -9,5 +9,11 @@ name=ARGV[2]
 json = File.open(file) { |f| JSON f.read }
 
 profiles=json['profiles']
-p = profiles.select{ |p| p['type'] == type and p['name'] == name }[0]
+ps = profiles.select{ |p| p['type'] == type and p['name'] == name }
+if (ps.nil? || ps.count == 0) 
+	puts "No profile found with name '#{name}' and type '#{type}' in '#{file}'"
+	profiles.each { |p| puts "#{p['type']} - #{p['name']}" }
+	exit -1
+end
+p = ps[0]
 puts p['uuid']
